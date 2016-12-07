448819cb8ba3ef4c206af081ad167f88ae5bdf6d
n3k1990 <n3k1990@gmail.com>
2016-12-07 09:01:27 -0300

    added header

--- prefetch/PrefetchASLRBypass.cpp
+++ prefetch/PrefetchASLRBypass.cpp
@@ -1,3 +1,20 @@
+// enrique.nissim@IOActive.com Copyright (C) 2016
+
+//Copyright(C) 2016 Enrique Nissim
+
+//This program is free software; you can redistribute it and/or
+//modify it under the terms of the GNU General Public License
+//as published by the Free Software Foundation.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with this program; if not, write to the Free Software
+//Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 // PrefetchASLRBypass.cpp : Defines the entry point for the console application.
 //
 
